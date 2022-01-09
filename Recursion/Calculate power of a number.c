2a1b40eaca67c70ba8a350b99be0603ddf511588
ANSHIKA <71966377+ANSHIKA1806@users.noreply.github.com>
2022-01-09 20:08:00 +0530

    Create Calculate power of a number.c

---
+++ Recursion/Calculate power of a number.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int power(int num, int pow){
+  int result=1;
+  if(pow<0)
+  {
+    for(int i=1;i<=pow;i++)
+      {
+    	result= result*num;
+      }
+    return (-result);
+  }
+  else{
+    for(int i=1;i<=pow;i++)
+  {
+    result= result*num;   
+  }
+  return result;
+  }
+}
+int main(){
+  int a,b;
+  scanf("%d%d",&a,&b);
+  printf("%d",power(a,b));
+  return 0;
+}
